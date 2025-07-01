5e6de8fe8c0a58d0996dc121b5edc2a8ef2e4cf9
Nekros981 <miguelsandovalg2018@gmail.com>
2025-06-30 17:21:10 -0700

    a expediente_clinico view se agrego el header y sun contenedor centrado con un en cabezado

---
+++ NutriMatic/domain/models/paciente.py
@@ -0,0 +1,20 @@
+import reflex as rx
+from sqlmodel import Field
+from datetime import date
+from typing import Optional
+
+class Paciente(rx.Model, table=True):
+    """
+    Representa un paciente en la base de datos.
+    Corresponde a la tabla 'pacientes'.
+    """
+    paciente_id: Optional[int] = Field(default=None, primary_key=True)
+    nombre_completo: str
+    fecha_nacimiento: Optional[date] = None
+    genero: Optional[str] = None   # tipo_genero ENUM en la BD
+    estado_civil: Optional[str] = None  # tipo_estado_civil ENUM en la BD
+    ocupacion: Optional[str] = None
+    telefono: Optional[str] = None
+    direccion: Optional[str] = None
+    nivel_educativo: Optional[str] = None
+    fecha_creacion: Optional[date] = Field(default_factory=date.today)
--- NutriMatic/ui/expediente_clinico/view/expediente_clinico_view.py
+++ NutriMatic/ui/expediente_clinico/view/expediente_clinico_view.py
@@ -9,6 +9,23 @@ def expediente_clinico_view() -> rx.Component:
         
         # --- Contenedor Principal ---
         rx.container(
-            
+            rx.vstack(
+                    rx.vstack(
+                    rx.heading("Expediente Clínico", size="4", text_align="center"),
+                    justify="center",
+                    align="center",
+                    width="100%",
+                ),
+            ),
+            # --- ESTILOS DEL CONTENEDOR PRINCIPAL (LA TARJETA BLANCA) ---
+            background_color="white",
+            padding="2rem",
+            border_radius="12px",
+            border="1px solid #e0e0e0",
+            box_shadow="0 2px 4px rgba(0, 0, 0, 0.1)",
+            margin_top="8rem",
+            margin_x="auto",    #  Centra el contenedor horizontalmente.
+            width="65%",       
+            max_width="90%", # Aumentado para hacerlo más grande.
         ),
     )
