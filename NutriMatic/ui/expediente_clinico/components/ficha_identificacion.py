0a854321fd7b0db8c231135f5d82391764e3a7fd
Nekros981 <miguelsandovalg2018@gmail.com>
2025-07-02 17:56:03 -0700

    agregue archivo de inputs en la carpea de componentes de expediente clinico y corrigi un nombre de un archivo

---
+++ NutriMatic/ui/expediente_clinico/components/ficha_identificacion.py
@@ -0,0 +1,10 @@
+import reflex as rx
+
+#from NutriMatic.ui.expediente_clinico.state.paciente_state import PacienteState
+
+def ficha_identificacion() -> rx.Component:
+    return rx.vstack(
+        rx.vstack(
+            
+        ),
+    )
