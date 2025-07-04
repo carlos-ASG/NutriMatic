beac2a000b945372df8d2c72686f4eeed975f7d7
Carlos Sandoval <syscarlos@outlook.com>
2025-07-04 11:13:23 -0700

    enum en ficha identificacion y migraciones hechas

--- NutriMatic/ui/expediente_clinico/components/ficha_identificacion.py
+++ NutriMatic/ui/expediente_clinico/components/ficha_identificacion.py
@@ -1,5 +1,7 @@
 from datetime import date, datetime
 import reflex as rx
+from NutriMatic.domain.models.expediente_clinico.enums.tipo_estado_civil import TipoEstadoCivil
+from NutriMatic.domain.models.expediente_clinico.enums.tipo_genero import TipoGenero
 from NutriMatic.ui.expediente_clinico.components.custom_inputs import input_field, select_field, form_section, fecha_input
 from NutriMatic.domain.models.expediente_clinico.paciente import Paciente
 
@@ -19,20 +21,25 @@ class FichaIdentificacionState(rx.State):
 
     @rx.var
     def to_paciente_model(self) -> Paciente:
-        """Convierte los atributos del estado a un objeto Paciente."""
+        """
+        Convierte los datos del formulario a un modelo Paciente.
+        """
         fecha_nacimiento_obj = None
         if self.fecha_nacimiento:
             try:
                 fecha_nacimiento_obj = datetime.strptime(self.fecha_nacimiento, "%Y-%m-%d").date()
             except ValueError:
-                # Manejar el caso de que la fecha no esté en el formato esperado
                 fecha_nacimiento_obj = None
+
+        # Convertir strings a Enums antes de crear el modelo
+        genero_enum = TipoGenero(self.genero) if self.genero else None
+        estado_civil_enum = TipoEstadoCivil(self.estado_civil) if self.estado_civil else None
 
         return Paciente(
             nombre_completo=self.nombre_completo,
             fecha_nacimiento=fecha_nacimiento_obj,
-            genero=self.genero,
-            estado_civil=self.estado_civil,
+            genero=genero_enum,
+            estado_civil=estado_civil_enum,
             ocupacion=self.ocupacion,
             telefono=self.telefono,
             direccion=self.direccion,
